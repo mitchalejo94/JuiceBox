3b73a13318b9bbffeee8f59cfe9b8e4b57037119
Mitchel Alejo <michalejo94@gmail.com>
2022-07-15 10:46:18 -0500

    Fourth Commit completed seeding

--- db/seed.js
+++ db/seed.js
@@ -3,17 +3,56 @@ const { client, getAllUsers } = require('./index');
 async function testDB () {
 
     try{
-        client.connect();
-
+        console.log("starting to test database");
         const users = await getAllUsers();
-        console.log(users);
+        console.log("get all users:",users);
+        console.log("finish testing DB");
 
     }
     catch(error){
-        console.error(error);
-    } finally{
-        client.end();
+        console.error("error testing database");
+        throw error
+    } 
+}
+// testDB();
+
+async function dropTables(){
+    try{
+        console.log("Starting to drop tables...");
+        await client.query(`
+        DROP TABLE IF EXISTS users;
+        `);
+        console.log('finish dropping tables');
+    }catch (error){
+        console.error("Error dropping tables!");
+        throw error;
     }
 }
 
-testDB();
+async function createTables(){
+    try{
+        console.log("starting to build tables...");
+        await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username varchar(255) UNIQUE NOT NULL,
+            password varchar(255) NOT NULL);
+        `);
+        console.log("finish building tables");
+    }catch (error){
+        console.error('error building tables');
+        throw error;
+    }
+}
+
+async function rebuildDB(){
+    try{
+        client.connect();
+
+        await dropTables();
+        await createTables ();
+    } catch (error){
+        throw error
+    }
+}
+rebuildDB().then(testDB).catch(console.error).finally(()=>{client.end()})
